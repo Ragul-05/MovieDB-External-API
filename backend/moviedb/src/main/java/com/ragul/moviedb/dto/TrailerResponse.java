0ec01e3f7ddd609537eea9fde204fdfca82d4700
Ragul-05 <ragulgopinath2005@gmail.com>
2026-03-18 18:49:34 +0530

    Add backend DTO layer changes

---
+++ backend/moviedb/src/main/java/com/ragul/moviedb/dto/TrailerResponse.java
@@ -0,0 +1,10 @@
+package com.ragul.moviedb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TrailerResponse {
+    private String youtubeUrl;
+}
